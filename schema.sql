ca19f244c8de26395e3313abbe9257710ac9ba15
hopeforkoami <hopeforkoami@gmail.com>
2023-01-24 20:51:58 +0000

    Add schema and queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+	id serial primary key,
+	name varchar(100) not null,
+	date_of_birth date not null,
+	escape_attempts integer default 0,
+	neutered boolean default false,
+	weight_kg numeric(12,2)
 );
