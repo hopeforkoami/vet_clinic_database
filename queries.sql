3f34ddc11d3698a4d1b5729ece58d611699e2147
hopeforkoami <hopeforkoami@gmail.com>
2023-01-28 05:25:58 +0000

    Add queries

--- queries.sql
+++ queries.sql
@@ -6,6 +6,8 @@ select name, escape_attempts from animals where weight_kg >10.5;
 select * from animals where neutered = true ;
 select * from animals where name != 'Gabumon';
 select * from animals where weight_kg >=10.4 and weight_kg <=17.3;
+
+
 begin;
 update animals set species = 'unspecified' ;
 rollback;
@@ -15,16 +17,23 @@ UPDATE animals SET species = 'digimon' WHERE name like '%mon' ;
 UPDATE animals SET species = 'pokemon' WHERE name not like '%mon' ;
 commit ; 
 select * from animals;
-begin;
 
+begin;
 delete from animals ; 
 rollback ; 
 select * from animals ; 
-begin ; 
 
+begin ; 
 delete from animals where date_of_birth > '2022-01-01';
 savepoint first_save_point ;
 update animals set weight_kg = weight_kg * (-1) ;
 rollback to savepoint first_save_point;
 update animals set weight_kg = weight_kg * (-1) where weight_kg <0 ;
 commit ; 
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) from animals WHERE date_of_birth BETWEEN '1990-01-01' and '2000-01-01' GROUP BY species;
