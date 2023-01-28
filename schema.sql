ef33fffbe0db77617f5621fcb1d198233936fe3e
hopeforkoami <hopeforkoami@gmail.com>
2023-01-28 07:55:16 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -12,3 +12,12 @@ CREATE TABLE animals (
 
 ALTER TABLE animals ADD COLUMN species  varchar(100);
 
+CREATE TABLE owner (id serial primary key,full_name varchar(255) not null);
+
+CREATE TABLE species (id serial primary key,name varchar(255) not null);
+
+alter table animals drop column species ;
+ALTER TABLE animals ADD COLUMN species_id  int;
+alter table animals add constraint fk_animal_species foreign key (species_id) references species(id);
+ALTER TABLE animals ADD COLUMN owner_id  int;
+alter table animals add constraint fk_animal_owner foreign key (owner_id) references owner(id);
