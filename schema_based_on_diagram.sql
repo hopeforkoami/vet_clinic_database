6e6caa9e2d395248822864555ef91b262179a26e
hopeforkoami <hopeforkoami@gmail.com>
2023-02-01 17:12:32 +0000

    Add remaining tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,8 +14,23 @@ admitted_at date not null,
 statut varchar(50) not null,
 patient_id int references patients(id)
 );
-CREATE TABLE medical_histories_treatments (
+CREATE TABLE medical_history_treatments (
 id serial primary key,
 medical_history_id int references medical_histories(id),
+treatment_id int references treatments(id)
+);
+CREATE TABLE invoices (
+id serial primary key,
+total_amount numeric(12,2),
+generated_at date not null,
+payed_at date,
+medical_history_id int references medical_histories(id)
+);
+CREATE TABLE invoice_items (
+id serial primary key,
+unit_price numeric(12,2),
+quantity int not null,
+total_price numeric(12,2),
+invoice_id int references invoices(id),
 treatment_id int references treatments(id)
 );
