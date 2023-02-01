ee00335dafa47e015226b311315b9b1c651719d1
hopeforkoami <hopeforkoami@gmail.com>
2023-02-01 16:57:38 +0000

    Add medical histories and treatment  relation
    Add medical histories and treatments many to many  relation table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,5 +6,16 @@ date_of_birth int not null
 CREATE TABLE treatments (
 id serial primary key,
 type varchar(255) not null,
-name varchar(255) not null,
+name varchar(255) not null
+);
+CREATE TABLE medical_histories (
+id serial primary key,
+admitted_at date not null,
+statut varchar(50) not null,
+patient_id int references patients(id)
+);
+CREATE TABLE medical_histories_treatments (
+id serial primary key,
+medical_history_id int references medical_histories(id),
+treatment_id int references treatments(id)
 );
